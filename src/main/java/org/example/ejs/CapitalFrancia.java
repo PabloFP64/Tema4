808be4e6317f063ba8a861ad3fe9ae582ac4ff1b
PabloFP64 <63953769+PabloFP64@users.noreply.github.com>
2023-02-26 13:00:31 +0100

    6

---
+++ src/main/java/org/example/ejs/CapitalFrancia.java
@@ -0,0 +1,31 @@
+package org.example.ejs;
+
+import java.util.Scanner;
+
+public class CapitalFrancia  {
+    //Ejercicio6
+    public void capital(Scanner teclado){
+        int numIntentos = 1;
+        String respuesta;
+        final int MAX_INTENTOS = 5;
+        teclado.nextLine();
+        System.out.println("¿Cuál es la capital de Francia? ");
+        respuesta=teclado.nextLine();
+        while (!respuesta.equals("Paris") && (MAX_INTENTOS - numIntentos != 0)){
+            System.out.println("Respuesta incorrecta");
+            System.out.println("Solo quedan " + (MAX_INTENTOS - numIntentos) + " intento(s)");
+            System.out.println("¿Cuál es la capital de Francia? ");
+            respuesta = teclado.nextLine();
+            numIntentos++;
+        }
+        if (MAX_INTENTOS - numIntentos != 0) {
+            System.out.println("Bravo!");
+        }else{
+            System.out.println("Revise sus conocimientos en geografía");
+        }
+
+
+    }
+
+
+}
