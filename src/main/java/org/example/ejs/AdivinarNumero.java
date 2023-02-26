d32ed842dd1142bfb0c66cdce86e35e7305bccc6
PabloFP64 <63953769+PabloFP64@users.noreply.github.com>
2023-02-26 12:58:40 +0100

    4

---
+++ src/main/java/org/example/ejs/AdivinarNumero.java
@@ -0,0 +1,27 @@
+package org.example.ejs;
+
+import java.util.Scanner;
+
+public class AdivinarNumero {
+    //Ejercicio4
+    public void adivinarNum(Scanner teclado){
+        int numero = (int) (Math.random()*100)+1;
+        System.out.println("¿En qué numero entre 1 y 100 estoy pensando?");
+        int entrada = teclado.nextInt();
+
+        while(entrada != numero){
+            if(entrada < numero){
+                System.out.println("Menor");
+                System.out.println("¿En qué numero entre 1 y 100 estoy pensando?");
+                entrada = teclado.nextInt();
+            }
+            else{
+                System.out.println("Mayor");
+                System.out.println("¿En qué numero entre 1 y 100 estoy pensando?");
+                entrada = teclado.nextInt();
+            }
+        }
+        System.out.println("¡Bravo, has acertado!");
+
+    }
+}
